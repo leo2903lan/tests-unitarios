de1af49fecfe79f496e12bb858f86df3599ae01a
Leandro Nogueira <leandro2903.lan@gmail.com>
2021-07-27 22:50:10 -0300

    atividade 5 , implementando função para reornar um objeto

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nome) => {
+  const avaliacao = () => 'Eita pessoa boa!';
+  return {
+    name: nome,
+    feedback: avaliacao,
+  };
+};
 
 module.exports = createStudent;
